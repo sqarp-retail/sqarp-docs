d6c5c5d03b24c0addfc824ee3c3b91d991044d63
lucasmolin <lucas@sqarp.com>
2023-10-18 17:57:32 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://sqarp-retail.github.io',
+  url: 'docs.sqarp.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
